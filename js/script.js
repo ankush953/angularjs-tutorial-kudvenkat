36dd6029ca04835b4875717f3eb71be93e3cadbf
Ankush Singh <ankusingh@paypal.com>
2021-07-18 16:28:15 +0530

    inline template example

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 /// <reference path="angular.min.js">
 
-var myApp = angular.module('main-app', []);
+var myApp = angular
+  .module("main-app", ["ngRoute"])
+  .config(function ($routeProvider) {
+      $routeProvider.when('/home',
+      {
+          templateUrl: "../templates/Home.html",
+          controller: "country-controller",
+      })
+      .when('/detail', {
+          templateUrl: "../templates/Detail.html",
+          controller: "city-controller"
+      })
+      .when('/inline', {
+          template: "<h1>You are viewing inline template</h1>",
+          controller: "state-controller"
+      })
+      .otherwise('/home');
+  });
